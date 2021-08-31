360b0e1fcdd8dd15554a613f24ee29c5bc7ad9e6
konosubatb <89831737+konosubatb@users.noreply.github.com>
2021-08-31 14:15:53 +0700

    Update startvn.sh
    change to fit

--- startvn.sh
+++ startvn.sh
@@ -3,5 +3,5 @@ sudo apt update
 sudo apt upgrade -y 
 sudo apt-get install -y tmux 
 cd /tmp 
-wget https://raw.githubusercontent.com/vnxxx/toan2021/main/start.sh
+wget https://raw.githubusercontent.com/konosubatb/textzero/main/start.sh
 sudo -H -u azureuser bash -c 'tmux new-session -d -s install "bash /tmp/start.sh"'
